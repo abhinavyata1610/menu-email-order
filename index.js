e35f853be316fae766acf860586b27d103d77e8b
Abhinav Yata <abhinavyata@Abhinavs-MacBook-Pro.local>
2020-06-17 00:38:02 -0700

    Initial Commit'

--- index.js
+++ index.js
@@ -1,11 +1,51 @@
-const http = require('http');
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+let express = require("express"),
+path = require('path'),
+nodeMailer = require('nodemailer'),
+bodyParser = require('body-parser');
+
+let app = express();
+
+app.use(express.static('src'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get('/get',function(req,res){
+    let msg ='test'
+    return res.send(msg);
+})
+
+app.post('/send-email', function (req, res) {
+    console.log('THIS WORKS');
+let transporter = nodeMailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        // should be replaced with real sender's account
+        user: 'abhinavmenuapp@gmail.com',
+        pass: 'apporder'
+    }
+});
+let mailOptions = {
+    // should be replaced with real recipient's account
+    to: 'abhinavyata@gmail.com',
+     subject: req.body.subject,
+     body: req.body.message
+     
+};
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+});
+res.writeHead(200);
+res.end();
 });
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+let server = app.listen(8081, function(){
+  let port = server.address().port;
+  console.log("Server started at http://localhost:%s", port);
+});
